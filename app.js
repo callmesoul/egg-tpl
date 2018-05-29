a62527ed1e373150278457dd94c81c679b59ab00
Yibin Chen <chenyibin@qtone.cn>
2018-05-29 11:34:08 +0800

    init

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ module.exports = app => {
     app.beforeStart(function* () {
         // 应用会等待这个函数执行完成才启动
         app.locals.tokenScrect = "JSON-WEB-TOKEN";
+        app.locals.defaultMsg = "操作失败，请稍后再试。";
         app.locals.wechat = { appId: "wx1e26b1652eed13a3", appSecret: "901c3d94bdf628c229ce34f7c823f060", token: "token" };
         app.model.sync();
     });
--- app/controller/order.js
+++ app/controller/order.js
@@ -4,19 +4,65 @@ const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
   async index(ctx) {
-      ctx.body="index";
+      let params = ctx.query;
+      let page=params.page?params.page:1;
+      let limit=params.limit?params.limit:12;
+      let offest=(page - 1) * limit;
+      let order=params.order?params.order:[['createAt', 'DESC']];
+      let result=await ctx.model.Order.findAndCountAll({where:params,limit:limit,offset:offest,order:order});
+      ctx.body={
+          dataList:result.rows,
+          pageParams:{
+              page:page,
+              pageTotal:Math.ceil(result.count/limit),
+              totalRow:result.count
+          }
+      }
   }
     async show(ctx) {
-        ctx.body="show";
+        let id=ctx.params.id;
+        ctx.body={
+            data:await ctx.model.Order.findById(id)
+        }
     }
-    async create(ctx) {
-        ctx.body="create";
+    async create(ctx) {//增
+      let result=await ctx.model.Order.create(ctx.body);
+      if(result){
+          ctx.body={
+              data:result
+          }
+      }else{
+          ctx.response.status = 400;
+          ctx.response.body = {
+              error:ctx.locals.defaultMsg
+          }
+      }
     }
     async update(ctx) {
-        ctx.body="update";
+        let result=await ctx.model.Order.update(ctx.body,{where:{id:ctx.params.id}});
+        if(result){
+            ctx.body={
+                data:"操作成功！"
+            }
+        }else{
+            ctx.response.status = 400;
+            ctx.response.body = {
+                error:ctx.locals.defaultMsg
+            }
+        }
     }
     async destroy(ctx) {
-        ctx.body="destroy";
+        let result=await ctx.model.Order.destroy({where:{id:ctx.params.id}});
+        if(result){
+            ctx.body={
+                data:"操作成功！"
+            }
+        }else{
+            ctx.response.status = 400;
+            ctx.response.body = {
+                error:ctx.locals.defaultMsg
+            }
+        }
     }
 }
 
